1d26d283b3335636e4fbb859f42c08b88099da51
jimmyNicholas <jimmynicholas@duck.com>
2024-09-10 21:52:45 +1000

    Added OptionsPanel to MainApp

--- src/_components/MainApp.tsx
+++ src/_components/MainApp.tsx
@@ -1,7 +1,10 @@
+import OptionsPanel from "./OptionsPanel";
+
 export default function MainApp() {
     return (
         <div className="border-2 border-black">
             Main App
+            <OptionsPanel/>
         </div>
     );
 };
