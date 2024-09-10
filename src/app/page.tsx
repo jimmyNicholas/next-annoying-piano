075a17237af66c08585c4c957ad5348b585b923e
jimmyNicholas <jimmynicholas@duck.com>
2024-09-10 10:14:50 +1000

    Added Header, MainApp and Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Header from "@/_layouts/Header";
+import MainApp from "@/_components/MainApp";
+import Footer from "@/_layouts/Footer";
+
 export default function Home() {
   return (
     <div>
       Annoying Piano
+      <Header/>
+      <MainApp/>
+      <Footer/>
     </div>
   );
 }
