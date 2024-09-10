103e8561194d5c3fc3dc6caca340e9e9a50761ca
jimmyNicholas <jimmynicholas@duck.com>
2024-09-10 22:04:21 +1000

    Imported and added options to OptionsPanel

--- src/_components/optionsPanel/OptionsPanel.tsx
+++ src/_components/optionsPanel/OptionsPanel.tsx
@@ -1,6 +1,15 @@
+import Global from "./Global";
+import Inputs from "./Inputs";
+import Modes from "./modes";
+import Outputs from "./Outputs";
+
 export default function OptionsPanel() {
     return (
-        <>
-        </>
+        <div>
+            <Global/>
+            <Inputs/>
+            <Modes/>
+            <Outputs/>
+        </div>
     );
 }
