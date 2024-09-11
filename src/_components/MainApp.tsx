a8f73ee50532676e0d20cd64d6eeb9467995d073
jimmyNicholas <jimmynicholas@duck.com>
2024-09-11 10:21:36 +1000

    imported and added Keyboard to Main App

--- src/_components/MainApp.tsx
+++ src/_components/MainApp.tsx
@@ -1,10 +1,12 @@
 import OptionsPanel from "./optionsPanel/OptionsPanel";
+import Keyboard from "./Keyboard";
 
 export default function MainApp() {
     return (
         <div className="border-2 border-black">
             Main App
             <OptionsPanel/>
+            <Keyboard/>
         </div>
     );
 };
