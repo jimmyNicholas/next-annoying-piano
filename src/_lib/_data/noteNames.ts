917d9fdf4fe09ec1acc74cdafafb011caa7db819
jimmyNicholas <jimmynicholas@duck.com>
2024-09-11 11:09:05 +1000

    Updated const notes to noteNames to improve readability

--- src/_lib/_data/noteNames.ts
+++ src/_lib/_data/noteNames.ts
@@ -1,2 +1,2 @@
 
-export const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+export const noteNames = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
