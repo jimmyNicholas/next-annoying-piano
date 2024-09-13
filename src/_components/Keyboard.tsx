e43fa8cc723fba3e6b163f0442c22e671e94954b
jimmyNicholas <jimmynicholas@duck.com>
2024-09-13 23:48:48 +1000

    Improved tailwind readability and added React fragments

--- src/_components/Keyboard.tsx
+++ src/_components/Keyboard.tsx
@@ -1,4 +1,5 @@
 import { Key } from '@/_lib/_types/types'
+import React from 'react';
 
 export default function Keyboard({
     keys,
@@ -15,18 +16,52 @@ export default function Keyboard({
                         whiteKeyMargin = '';
                     }
                     return (
-                        <>
+                        <React.Fragment key={key.name}> 
                             {key.pitch[1] === '#' ? 
                                 //black key
-                                <button className=' relative float-left h-[4em] w-[1em] z-[1] -ml-[0.7em] border-[1px] border-black rounded-b-[3px] shadow-[inset_-1px_-1px_2px_rgba(255,255,255,0.2),inset_0_-5px_2px_3px_rgba(0,0,0,0.6),0_2px_4px_rgba(0,0,0,0.5)] bg-gradient-to-br from-[#222] to-[#555]  
-                                                    active:shadow-[inset_-1px_-1px_2px_rgba(255,255,255,0.2),inset_0_-2px_2px_3px_rgba(0,0,0,0.6),inset_0_1px_2px_rgba(0,0,0,0.5)] active:bg-gradient-to-r from-[#444] to-[#222]'>
+                                <button
+                                    className=' 
+                                        relative 
+                                        float-left 
+                                        h-[4em] 
+                                        w-[1em] 
+                                        z-[1] 
+                                        -ml-[0.7em] 
+                                        border-[1px] 
+                                        border-black 
+                                        rounded-b-[3px] 
+                                        shadow-[inset_-1px_-1px_2px_rgba(255,255,255,0.2),inset_0_-5px_2px_3px_rgba(0,0,0,0.6),0_2px_4px_rgba(0,0,0,0.5)] 
+                                        bg-gradient-to-br from-[#222] to-[#555]  
+                                        active:shadow-[inset_-1px_-1px_2px_rgba(255,255,255,0.2),inset_0_-2px_2px_3px_rgba(0,0,0,0.6),inset_0_1px_2px_rgba(0,0,0,0.5)] 
+                                        active:bg-gradient-to-r from-[#444] to-[#222]'
+                                >
                                 </button> 
                                 : 
                                 //white key
-                                <button className={`    relative float-left h-[8em] w-[2em] z-[0.5] ${whiteKeyMargin} border-l-[1px] border-b-[1px] border-[#bbb] rounded-b-[5px] shadow-[inset_-1px_0_0_rgba(255,255,255,0.8),inset_0_0_5px_#ccc,inset_0_0_3px_rgba(0,0,0,0.2)] bg-gradient-to-b from-[#eee] to-white whiteKey-active 
-                                                        active:border-t-[1px] active:border-l-[1px] active:border-b-[1px] active:border-[#999] active:shadow-[inset_2px_0_3px_rgba(0,0,0,0.1),inset_-5px_5px_20px_rgba(0,0,0,0.2),inset_0_0_3px_rgba(0,0,0,0.2)] active:bg-gradient-to-b from-white to-[#e9e9e9]`}>
+                                <button 
+                                    key={key.name} 
+                                    className={`    
+                                        relative 
+                                        float-left 
+                                        h-[8em] 
+                                        w-[2em] 
+                                        z-[0.5] 
+                                        ${whiteKeyMargin} 
+                                        border-l-[1px] 
+                                        border-b-[1px] 
+                                        border-[#bbb] 
+                                        rounded-b-[5px] 
+                                        shadow-[inset_-1px_0_0_rgba(255,255,255,0.8),inset_0_0_5px_#ccc,inset_0_0_3px_rgba(0,0,0,0.2)] 
+                                        bg-gradient-to-b 
+                                        from-[#eee] to-white 
+                                        active:border-t-[1px] 
+                                        active:border-l-[1px] 
+                                        active:border-b-[1px] 
+                                        active:border-[#999] 
+                                        active:shadow-[inset_2px_0_3px_rgba(0,0,0,0.1),inset_-5px_5px_20px_rgba(0,0,0,0.2),inset_0_0_3px_rgba(0,0,0,0.2)] 
+                                        active:bg-gradient-to-b from-white to-[#e9e9e9]`}>
                                 </button>}
-                        </>
+                        </React.Fragment> 
                     )
                 })}
             </div>
