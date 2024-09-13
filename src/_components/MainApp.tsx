e1648fe432e98d6aef3f43d4c975ac4329d162c1
jimmyNicholas <jimmynicholas@duck.com>
2024-09-13 16:31:47 +1000

    Added keys const and Keyboard params

--- src/_components/MainApp.tsx
+++ src/_components/MainApp.tsx
@@ -1,12 +1,17 @@
 import OptionsPanel from "./optionsPanel/OptionsPanel";
 import Keyboard from "./Keyboard";
+import { getKeys } from "@/_utils/keys/keyboardSetup";
 
 export default function MainApp() {
+    const keys = getKeys('C', 2, 'C', 4);
+
     return (
         <div className="border-2 border-black">
             Main App
             <OptionsPanel/>
-            <Keyboard/>
+            <Keyboard
+                keys={keys}
+            />
         </div>
     );
 };
