2079b17521289062089aa29d459c9d505ea718a1
jimmyNicholas <jimmynicholas@duck.com>
2024-09-14 16:05:07 +1000

    Added synth, now and play / stop hertz functions

--- src/_services/audio.ts
+++ src/_services/audio.ts
@@ -3,3 +3,14 @@ import * as Tone from "tone";
 export async function setupAudio() {
     await Tone.start();
 }
+
+const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+const now = Tone.now();
+
+export function playHertz(hertz: number) {
+    synth.triggerAttack(hertz, now);
+}
+
+export function stopHertz(hertz: number) {
+    synth.triggerRelease(hertz, now)
+}
