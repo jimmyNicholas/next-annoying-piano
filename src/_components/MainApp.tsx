6b290fbb828aa594a36771fb8531e0350f653314
jimmyNicholas <jimmynicholas@duck.com>
2024-09-14 16:02:08 +1000

    Added setup audio to start tone.js

--- src/_components/MainApp.tsx
+++ src/_components/MainApp.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import OptionsPanel from "./optionsPanel/OptionsPanel";
 import Keyboard from "./Keyboard";
 import { getKeys } from "@/_utils/keys/keyboardSetup";
+import { useEffect } from "react";
+import { setupAudio } from "@/_services/audio";
 
 export default function MainApp() {
     const keys = getKeys('C', 2, 'B', 4);
+
+    useEffect(() => {
+        setupAudio();
+    }, []);  
 
     return (
         <div className="border-2 border-black">
---
+++ src/_services/audio.ts
@@ -0,0 +1,5 @@
+import * as Tone from "tone";
+
+export async function setupAudio() {
+    await Tone.start();
+}
