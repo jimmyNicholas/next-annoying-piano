183f54ad85c6ad8e61aeb111b450da10407f6af4
jimmyNicholas <jimmynicholas@duck.com>
2024-09-14 22:01:16 +1000

    Added dynamic import integration for enabling audio and play and stop notoes

--- src/_components/MainApp.tsx
+++ src/_components/MainApp.tsx
@@ -3,19 +3,38 @@
 import OptionsPanel from "./optionsPanel/OptionsPanel";
 import Keyboard from "./Keyboard";
 import { getKeys } from "@/_utils/keys/keyboardSetup";
-import { useEffect } from "react";
-import { setupAudio } from "@/_services/audio";
+
 
 export default function MainApp() {
     const keys = getKeys('C', 2, 'B', 4);
 
-    useEffect(() => {
-        setupAudio();
-    }, []);  
-
     return (
         <div className="border-2 border-black">
-            Main App
+            Main App 
+            <button 
+                onClick={async () => {
+                    const {setupAudio} = await import('../_services/audio');
+                    await setupAudio();
+                }}
+            >
+                -Enable Audio-
+            </button>
+            <button 
+                onClick={async () => {
+                    const {playHertz} = await import('../_services/audio');
+                    playHertz(440);
+                }}
+            >
+                -Test Audio-
+            </button>
+            <button 
+                onClick={async () => {
+                    const {stopHertz} = await import('../_services/audio');
+                    stopHertz(440);
+                }}
+            >
+                -Stop Audio-
+            </button>
             <OptionsPanel/>
             <Keyboard
                 keys={keys}
