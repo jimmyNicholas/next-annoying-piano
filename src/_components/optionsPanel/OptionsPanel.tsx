e486130da8b8f32398c1686630f86e3698eb8a04
jimmyNicholas <jimmynicholas@duck.com>
2024-09-15 21:56:43 +1000

    Added onReset to UI with basic styling and tested reset function

--- src/_components/optionsPanel/OptionsPanel.tsx
+++ src/_components/optionsPanel/OptionsPanel.tsx
@@ -4,15 +4,19 @@ import Modes from "./Modes";
 import Outputs from "./Outputs";
 
 export default function OptionsPanel({
+    onReset,
     mode,
     updateMode,
 } : {
+    onReset: () => void,
     mode: string;
     updateMode: (mode: string) => void;
 }) {
     return (
         <div className="grid grid-cols-[15%_20%_45%_20%]">
-            <Global/>
+            <Global
+                onReset={onReset}
+            />
             <Inputs/>
             <Modes
                 mode={mode}
