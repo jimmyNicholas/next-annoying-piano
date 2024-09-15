ee6c610d7b04e5a032e54bfb8e1e2698ebb35b5b
jimmyNicholas <jimmynicholas@duck.com>
2024-09-15 12:30:00 +1000

    Added pitch->hertz conversion

--- src/_utils/hertzHelpers.ts
+++ src/_utils/hertzHelpers.ts
@@ -1,3 +1,4 @@
+import { notePitches } from "@/_lib/_data/noteNames";
 
 function calculateHertz(baseHertz: number, interval: number) {
     if (interval === 0) {
@@ -6,3 +7,21 @@ function calculateHertz(baseHertz: number, interval: number) {
     const ratio = Math.pow(2, 1 / 12);
     return Math.round(baseHertz * Math.pow(ratio, interval));
 }
+
+function getInterval(targetName: string, targetOctave: number, baseName: string, baseOctave: number) {
+    const baseIndex = notePitches.indexOf(baseName);
+    const targetIndex = notePitches.indexOf(targetName);
+    if (baseOctave === targetOctave) {
+        return targetIndex - baseIndex;
+    }
+    const octaveInterval = (targetOctave - baseOctave) * 12;
+    return targetIndex - baseIndex + octaveInterval;
+}
+
+export function convertPitchToHertz(targetName: string, targetOctave: number) {
+    const baseName: string = 'A';
+    const baseOctave: number = 4;
+    const baseHertz: number = 440;
+    const interval = getInterval(targetName, targetOctave, baseName, baseOctave);
+    return calculateHertz(baseHertz, interval);
+};
