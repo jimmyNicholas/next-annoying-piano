15d19b224a02fcbe7e77d75ef97f337f47e5ddc7
jimmyNicholas <jimmynicholas@duck.com>
2024-09-17 09:57:32 +1000

    Cleaned up return types

--- src/_utils/hertzHelpers.ts
+++ src/_utils/hertzHelpers.ts
@@ -1,7 +1,7 @@
 import { notePitches } from "@/_lib/_data/noteNames";
 import { KeyboardRange, HertzTable } from "@/_lib/_types/types";
 
-function calculateHertz(baseHertz: number, interval: number) {
+function calculateHertz(baseHertz: number, interval: number): number {
     if (interval === 0) {
         return baseHertz;
     }
@@ -9,7 +9,7 @@ function calculateHertz(baseHertz: number, interval: number) {
     return Math.round(baseHertz * Math.pow(ratio, interval));
 }
 
-function getInterval(targetName: string, targetOctave: number, baseName: string, baseOctave: number) {
+function getInterval(targetName: string, targetOctave: number, baseName: string, baseOctave: number): number {
     const baseIndex = notePitches.indexOf(baseName);
     const targetIndex = notePitches.indexOf(targetName);
     if (baseOctave === targetOctave) {
@@ -19,7 +19,7 @@ function getInterval(targetName: string, targetOctave: number, baseName: string,
     return targetIndex - baseIndex + octaveInterval;
 }
 
-function convertPitchToHertz(targetName: string, targetOctave: number) {
+function convertPitchToHertz(targetName: string, targetOctave: number): number {
     const baseName: string = 'A';
     const baseOctave: number = 4;
     const baseHertz: number = 440;
