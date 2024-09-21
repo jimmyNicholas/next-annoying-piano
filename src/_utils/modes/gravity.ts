869b90dd14901127a668a658d7dbbe31ac0fa40a
jimmyNicholas <jimmynicholas@duck.com>
2024-09-21 13:44:07 +1000

    Added modifyer into function

--- src/_utils/modes/gravity.ts
+++ src/_utils/modes/gravity.ts
@@ -1,11 +1,11 @@
 import { HertzModifyers, HertzTable } from "@/_lib/_types/types";
 
 const gravity = (hertzModifiers: HertzModifyers, hertzTable: HertzTable): void => {
-    const { lastKey, currentKey } = hertzModifiers;
+    const { lastKey, currentKey, modifyerOne } = hertzModifiers;
     
     const lastHertz = hertzTable[lastKey];
     const currentHertz = hertzTable[currentKey];
-    const newHertz = Math.round(lastHertz - ((lastHertz - currentHertz) / 20));
+    const newHertz = Math.round(lastHertz - ((lastHertz - currentHertz) / modifyerOne));
     hertzTable[lastKey] = newHertz;
 };
 
