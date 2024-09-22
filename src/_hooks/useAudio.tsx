6083889890a26d3c171fa6f55277aa1299e3ae89
jimmyNicholas <jimmynicholas@duck.com>
2024-09-22 16:31:11 +1000

    Added loadAudio function, returns and export

--- src/_hooks/useAudio.tsx
+++ src/_hooks/useAudio.tsx
@@ -5,5 +5,17 @@ function useAudio() {
     const [audioIsLoaded, setAudioIsLoaded] = useState<boolean>(false);
     const [audioService, setAudioService] = useState<AudioModule | null>(null);
 
+    useEffect(() => {
+        async function loadAudio() {     
+            const {audioModule} = await import('../_services/audio');
+            await audioModule.setupAudio()
+                .then(() => setAudioIsLoaded(true))
+                .then(() => setAudioService(audioModule));    
+        };
+        loadAudio();
+    }, []);
 
-}
+    return { audioIsLoaded, audioService };
+}   
+
+export default useAudio;
