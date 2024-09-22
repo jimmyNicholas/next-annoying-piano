d73b82a7e71a13402302a64800ef0cbd59960436
jimmyNicholas <jimmynicholas@duck.com>
2024-09-22 17:07:42 +1000

    Added imports and declarations

--- src/_hooks/useKeyboard.tsx
+++ src/_hooks/useKeyboard.tsx
@@ -1,6 +1,13 @@
-
+import { useState, useRef } from "react";
+import { Key, HertzTable } from "@/_lib/_types/types";
+import { getKeys } from "@/_utils/keys/keyboardSetup";
+import { getHertzTable } from "@/_utils/hertzHelpers";
 
 function useKeyboard() {
+    const keyboardRange = {startPitch: 'C', startOctave: 2, endPitch: 'B', endOctave: 4};
+    const [keys] = useState<Key[]>( getKeys( keyboardRange) );
+    const hertzTable = useRef<HertzTable>(getHertzTable( keyboardRange ));
+    const lastReleased = useRef<string | null>(null);
 
 };
 
