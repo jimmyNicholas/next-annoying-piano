2cc8c3b9bb68613f6322399696fcaf64e1ed6de9
jimmyNicholas <jimmynicholas@duck.com>
2024-09-22 13:03:50 +1000

    Refactored Inputs and added props

--- src/_components/optionsPanel/Inputs.tsx
+++ src/_components/optionsPanel/Inputs.tsx
@@ -1,7 +1,14 @@
-export default function Inputs() {
+import { InputProps } from "@/_lib/_types/types";
+
+const Inputs: React.FC<InputProps> = (
+    isQwertyEnabled,
+    toggleIsQwertyEnabled
+) => {
     return (
         <div className="border-2 border-black">
             Inputs
         </div>
     );
-}
+};
+
+export default Inputs;
