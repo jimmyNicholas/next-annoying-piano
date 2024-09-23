90b1d62df891950d179d809960d48878ef026916
jimmyNicholas <jimmynicholas@duck.com>
2024-09-23 22:26:11 +1000

    Simplified setup func further

--- src/_services/midiController.ts
+++ src/_services/midiController.ts
@@ -1,19 +1,16 @@
 import { WebMidi, Input } from "webmidi";
-import { useState } from 'react';
 
 const getMidiControllerInputs = () => {
     const inputs: Input[] = WebMidi.inputs;
     return inputs;
 };
 
-const setupMidiController = async () => { 
-    const [midiLoaded, setMidiLoaded] = useState(false);
-
-    await WebMidi.enable()  
-        .then(() => setMidiLoaded(true))
-        .catch(err => alert(err))
-
-    return midiLoaded;
+const setupMidiController = () => { 
+    try {
+        WebMidi.enable();
+    } catch (err) {
+        throw err;
+    }
 };
 
 export {setupMidiController, getMidiControllerInputs};
