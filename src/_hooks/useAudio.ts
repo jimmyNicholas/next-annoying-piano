8461d1cbdef847b2708d1063d9618cf35b666ca7
jimmyNicholas <jimmynicholas@duck.com>
2024-10-07 22:41:52 +1100

    Added now and polySynth hooks for updated playHertz and stopHertz functions

--- src/_hooks/useAudio.ts
+++ src/_hooks/useAudio.ts
@@ -1,19 +1,22 @@
-import { useCallback, useEffect, useState } from 'react';
-import { AudioModule } from "@/_lib/_types/types";
+import { useCallback, useEffect, useRef, useState } from 'react';
+import * as ToneType from 'tone';
+import { Note } from '@/_lib/_types/types'
 
 const useAudio = () => {
+    // setup audio with Tonejs
     const [audioIsLoaded, setAudioIsLoaded] = useState<boolean>(false);
-    const [audioService, setAudioService] = useState<AudioModule | null>(null);
-    const [ tone, setTone ] = useState({});
-
+    const [ tone, setTone ] = useState<typeof ToneType | null>(null);
+    
     const startAudio = useCallback(async () => {
         if (!audioIsLoaded) {
-            const Tone = await import('tone');
+            const Tone = await import('tone') as typeof ToneType;
             setTone(Tone);
             await Tone.start()
-                .then(() => setAudioIsLoaded(true));
+                .then(() => {
+                    setAudioIsLoaded(true)
+                });
         };
-    }, [audioIsLoaded, setTone]);
+    }, [audioIsLoaded]);
 
     useEffect(() => {
         function setupAudio() {
@@ -27,14 +30,38 @@ const useAudio = () => {
         setupAudio();
     }, [startAudio]);    
 
-    async function loadAudio() {     
-        const {audioModule} = await import('../_services/audio');
-        await audioModule.setupAudio()
-            .then(() => setAudioIsLoaded(true))
-            .then(() => setAudioService(audioModule));    
-    };
+    const [ now, setNow ] = useState<number | null>(null);
+    const [ polySynth, setPolySynth] = useState<ToneType.PolySynth | null>(null);
+    const playingNotes = useRef<Note[]>([]);
 
-    return { loadAudio, audioIsLoaded, audioService };
+    useEffect(() => { 
+        if (!audioIsLoaded || !tone) return;
+        setNow(tone.now());
+        const polySynth = new tone.PolySynth(tone.Synth).toDestination();
+        setPolySynth(polySynth);
+    }, [audioIsLoaded])
+
+    const playHertz = useCallback((keyName: string, hertz: number): void => {
+        if (!audioIsLoaded || !now || !polySynth) return;
+        polySynth.triggerAttack(hertz, now);
+        playingNotes.current.push({keyName, hertz})
+    }, [audioIsLoaded, now, polySynth]);
+
+    const stopHertz = useCallback((keyName: string): void => {
+        if (!audioIsLoaded || !now || !polySynth) return;
+        const currentNote = playingNotes.current.find((playingNote) => {return playingNote.keyName === keyName });
+        if (typeof currentNote === 'undefined') return;
+        polySynth.triggerRelease(currentNote.hertz, now);
+
+        playingNotes.current = playingNotes.current.filter(
+            playingNote => playingNote !== currentNote
+        );
+        
+    }, [audioIsLoaded, now, polySynth]);
+
+    const audioService = { playHertz, stopHertz};
+
+    return { audioIsLoaded, audioService};
 }   
 
 export default useAudio;
