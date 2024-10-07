a33dadafe3fb1fd6a08efe4dfe67f40c2ff422b2
jimmyNicholas <jimmynicholas@duck.com>
2024-10-07 21:19:27 +1100

    Updated tone start function to use more efficient hooks

--- src/_hooks/useAudio.ts
+++ src/_hooks/useAudio.ts
@@ -1,9 +1,31 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { AudioModule } from "@/_lib/_types/types";
 
-function useAudio() {
+const useAudio = () => {
     const [audioIsLoaded, setAudioIsLoaded] = useState<boolean>(false);
     const [audioService, setAudioService] = useState<AudioModule | null>(null);
+    const [ tone, setTone ] = useState({});
+
+    const startAudio = useCallback(async () => {
+        if (!audioIsLoaded) {
+            const Tone = await import('tone');
+            setTone(Tone);
+            await Tone.start()
+                .then(() => setAudioIsLoaded(true));
+        };
+    }, [audioIsLoaded, setTone]);
+
+    useEffect(() => {
+        function setupAudio() {
+            window.addEventListener('click', startAudio);
+            window.addEventListener('keydown', startAudio);
+            return () => {
+                window.removeEventListener('click', () => setAudioIsLoaded(false));
+                window.removeEventListener('keydown', () => setAudioIsLoaded(false))
+            };
+        };
+        setupAudio();
+    }, [startAudio]);    
 
     async function loadAudio() {     
         const {audioModule} = await import('../_services/audio');
