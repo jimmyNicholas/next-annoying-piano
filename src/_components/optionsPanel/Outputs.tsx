982194d2f7cc83c5d0ec17c8ef028f7c0c917bff
jimmyNicholas <jimmynicholas@duck.com>
2024-10-13 09:41:30 +1100

    Added effects nodes prop

--- src/_components/optionsPanel/Outputs.tsx
+++ src/_components/optionsPanel/Outputs.tsx
@@ -1,7 +1,14 @@
-export default function Outputs() {
+import { OutputProps } from "@/_lib/_types/types";
+
+const Outputs: React.FC<OutputProps> = ({
+    effectsNodes
+}) => {
+    console.log(effectsNodes);
     return (
         <div className="border-2 border-black">
             Outputs
         </div>
     );
-}
+};
+
+export default Outputs;
