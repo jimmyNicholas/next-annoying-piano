37d32a24845ae6d94b0e69e17596b6627baa722e
jimmyNicholas <jimmynicholas@duck.com>
2024-10-13 09:39:47 +1100

    Updated effects options to nodes

--- src/_hooks/useAudio.ts
+++ src/_hooks/useAudio.ts
@@ -3,9 +3,9 @@ import useHertzPlayback from './audioHooks/useHertzPlayback';
 import { ToneType } from '@/_lib/_types/types';
 
 const useAudio = (tone: typeof ToneType | null ) => {
-    const { polySynth, effectsOptions } = useConnectEffects(tone);
+    const { polySynth, effectsNodes } = useConnectEffects(tone);
     const { hertzPlayback } = useHertzPlayback(tone, polySynth);
-    return { tone, hertzPlayback, effectsOptions}
+    return { tone, hertzPlayback, effectsNodes}
 };
 
 export default useAudio;
