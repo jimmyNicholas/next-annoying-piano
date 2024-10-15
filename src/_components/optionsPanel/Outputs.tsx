d1fe6a65759b95bbaa9a6517f41554b3fece2b3d
jimmyNicholas <jimmynicholas@duck.com>
2024-10-16 09:13:43 +1100

    Removed debug log

--- src/_components/optionsPanel/Outputs.tsx
+++ src/_components/optionsPanel/Outputs.tsx
@@ -3,7 +3,6 @@ import { OutputProps } from "@/_lib/_types/types";
 const Outputs: React.FC<OutputProps> = ({
     effectsNodes
 }) => {
-    console.log(effectsNodes);
     return (
         <div className="border-2 border-black">
             Outputs
