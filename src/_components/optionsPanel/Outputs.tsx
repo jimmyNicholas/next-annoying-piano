3ef4dcc93f42bd6bf79f23f81dee08ec3c21935e
jimmyNicholas <jimmynicholas@duck.com>
2024-10-22 09:44:48 +1100

    Updated the state and integrated the interface

--- src/_components/optionsPanel/Outputs.tsx
+++ src/_components/optionsPanel/Outputs.tsx
@@ -1,31 +1,25 @@
-import { OutputProps, ReverbInterface } from "@/_lib/_types/types";
-import { useCallback, useEffect, useState } from "react";
+import { OutputProps, EffectInterface } from "@/_lib/_types/types";
+import { useCallback, useState } from "react";
 
 const Outputs: React.FC<OutputProps> = ({
     effectsInterfaces
 }) => {
     const {gainInterface, reverbInterface, vibratoInterface} = effectsInterfaces;
     
-    const getValueFromName = useCallback((valueName: string, effectInterface: ReverbInterface) => {
+    const getValueFromName = useCallback((valueName: string, effectInterface: EffectInterface) => {
         return effectInterface.options.find(({name}) => name === valueName)?.get();
     }, []);
 
     const [reverb, setReverb] = useState<{[key: string]: number | undefined}>({});
-    
-    const [vibrato, setVibrato] = useState({
-        frequency: vibratoInterface?.get()?.frequency ?? 0,
-        depth: vibratoInterface.get()?.depth ?? 0,
-        wet: vibratoInterface.get()?.wet ?? 0
-    });
-    
-    const [gain, setGain] = useState<number>( gainInterface.get()?.gain ?? 0 );
+    const [vibrato, setVibrato] = useState<{[key: string]: number | undefined}>({});
+    const [gain, setGain] = useState<{[key: string]: number | undefined}>({});
 
     return (
         <div className="border-2 border-black">
             {reverbInterface.name}
             {reverbInterface.options.map((option) => (
                 <div key={reverbInterface.name + option.name} className="grid grid-cols-[25%_65%_10%]">
-                    {option.name}
+                    {option.title}
                     <input 
                         type="range"
                         className="w-full"
@@ -43,70 +37,44 @@ const Outputs: React.FC<OutputProps> = ({
             ))}
             
             {vibratoInterface.name}
-            <div key={'vibratoFrequency'} className="grid grid-cols-[25%_65%_10%]">
-                Frequency:
-                <input 
-                    type="range"
-                    className="w-full"
-                    value={vibrato.frequency}
-                    min={0}
-                    max={100}
-                    onChange={(e) => {
-                        vibratoInterface.set({frequency: Number(e.target.value)})
-                        setVibrato({...vibrato, frequency: Number(e.target.value)});
-                    }}                        
-                />
-                {vibratoInterface.get()?.frequency}
-            </div>
-            <div key={'vibratoDepth'} className="grid grid-cols-[25%_65%_10%]">
-                Depth:
-                <input 
-                    type="range"
-                    className="w-full"
-                    value={vibrato.depth}
-                    min={0}
-                    max={1}
-                    step={0.01}
-                    onChange={(e) => {
-                        vibratoInterface.set({depth: Number(e.target.value)})
-                        setVibrato({...vibrato, depth: Number(e.target.value)});
-                    }}                        
-                />
-                {vibratoInterface.get()?.depth}
-            </div>
-            <div key={'vibratoWet'} className="grid grid-cols-[25%_65%_10%]">
-                Wet:
-                <input 
-                    type="range"
-                    className="w-full"
-                    value={vibrato.wet}
-                    min={0}
-                    max={1}
-                    step={0.01}
-                    onChange={(e) => {
-                        vibratoInterface.set({wet: Number(e.target.value)})
-                        setVibrato({...vibrato, wet: Number(e.target.value)});
-                    }}                        
-                />
-                {vibratoInterface.get()?.wet}
-            </div>
+            {vibratoInterface.options.map((option) => (
+                <div key={vibratoInterface.name + option.name} className="grid grid-cols-[25%_65%_10%]">
+                    {option.title}
+                    <input 
+                        type="range"
+                        className="w-full"
+                        value={getValueFromName(option.name, vibratoInterface)}
+                        min={option.min}
+                        max={option.max}
+                        step={option.step}
+                        onChange={(e) => {
+                            option.set(Number(e.target.value));
+                            setVibrato({...vibrato, [option.name]: Number(e.target.value)})
+                        }}                        
+                    />
+                    {option.get()}
+                </div>
+            ))}
+
             {gainInterface.name}
-            <div key={gainInterface.name} className="grid grid-cols-[25%_65%_10%]">
-                Volume:
-                <input 
-                    type="range"
-                    className="w-full"
-                    value={gain}
-                    min={0}
-                    max={1}
-                    step={0.01}
-                    onChange={(e) => {
-                        gainInterface.set({gain: Number(e.target.value)})
-                        setGain(Number(e.target.value));
-                    }}                        
-                />
-                {gainInterface.get()?.gain}
-            </div>
+            {gainInterface.options.map((option) => (
+                <div key={gainInterface.name + option.name} className="grid grid-cols-[25%_65%_10%]">
+                    {option.title}
+                    <input 
+                        type="range"
+                        className="w-full"
+                        value={getValueFromName(option.name, gainInterface)}
+                        min={option.min}
+                        max={option.max}
+                        step={option.step}
+                        onChange={(e) => {
+                            option.set(Number(e.target.value));
+                            setGain({...gain, [option.name]: Number(e.target.value)})
+                        }}                        
+                    />
+                    {option.get()}
+                </div>
+            ))}
         </div>
     );
 };
