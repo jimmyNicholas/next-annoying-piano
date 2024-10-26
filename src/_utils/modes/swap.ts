83612208f7449a31ea25cd92172dfd9bffa9e9a4
jimmyNicholas <jimmynicholas@duck.com>
2024-10-26 11:15:27 +1100

    Updated swap mode to a class

--- src/_utils/modes/swap.ts
+++ src/_utils/modes/swap.ts
@@ -1,19 +1,20 @@
+import { Mode } from "@/_lib/_data/modes/Mode";
 import { HertzModifiers, HertzTable } from "@/_lib/_types/types";
 
-const modifySwap = (hertzModifiers: HertzModifiers, hertzTable: HertzTable): void => {
-    const { lastKey, currentKey } = hertzModifiers;
+export class SwapMode extends Mode {
+    constructor() {
+        super(
+            'Swap',
+            'Swaps pitch of the last released key with the previous key.',
+            []
+        );
+    };
 
-    const lastHertz = hertzTable[lastKey];
-    const currentHertz = hertzTable[currentKey];
-    hertzTable[lastKey] = currentHertz;
-    hertzTable[currentKey] = lastHertz;
+    modify(hertzModifiers: HertzModifiers, hertzTable: HertzTable): void {
+        const { lastKey, currentKey } = hertzModifiers;
+        const lastHertz = hertzTable[lastKey];
+        const currentHertz = hertzTable[currentKey];
+        hertzTable[lastKey] = currentHertz;
+        hertzTable[currentKey] = lastHertz;
+    };
 };
-
-const swapMode = {
-    name: 'Swap',
-    description: "Swaps pitch of the last released key with the previous key.",
-    modify: modifySwap,
-    modifiers: []
-};
-
-export default swapMode;
