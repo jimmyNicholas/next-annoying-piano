9089b456cd4670b87951ebaab042bc7d83cc0725
jimmyNicholas <jimmynicholas@duck.com>
2024-10-26 11:11:20 +1100

    updated gravity mode to class

--- src/_utils/modes/gravity.ts
+++ src/_utils/modes/gravity.ts
@@ -1,15 +1,31 @@
 import { HertzModifiers, HertzTable } from "@/_lib/_types/types";
+import { Mode } from "@/_lib/_data/modes/Mode";
 
-const gravity = (hertzModifiers: HertzModifiers, hertzTable: HertzTable): void => {
-    const { lastKey, currentKey, modifiers } = hertzModifiers;
-    if (!modifiers) return;
+export class GravityMode extends Mode {
+    constructor() {
+        super(
+            'Gravity',
+            'Pulls the pitch of the previous key toward it the last released key.',
+            [
+                {
+                    modName: 'STRENGTH',
+                    label: 'Strength',
+                    min: 1,
+                    value: 2,
+                    max: 10,
+                    step: 0.1
+                }
+            ]
+        )
+    }
 
-    const lastHertz = hertzTable[lastKey];
-    const currentHertz = hertzTable[currentKey];
-    const strength = modifiers.find((mod) => {return mod.modName === 'STRENGTH'});
-    if (!strength) return;
-    const newHertz = Math.round(lastHertz - ((lastHertz - currentHertz) / (strength.max - strength.value)));
-    hertzTable[lastKey] = newHertz;    
+    modify(hertzModifiers: HertzModifiers, hertzTable: HertzTable): void {
+        const { lastKey, currentKey } = hertzModifiers;
+        const lastHertz = hertzTable[lastKey];
+        const currentHertz = hertzTable[currentKey];
+        const strength = this.modifiers.find((mod) => {return mod.modName === 'STRENGTH'});
+        if (!strength) return;
+        const newHertz = Math.round(lastHertz - ((lastHertz - currentHertz) / (strength.max - strength.value)));
+        hertzTable[lastKey] = newHertz;  
+    }
 };
-
-export default gravity;
