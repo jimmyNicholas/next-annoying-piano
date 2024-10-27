c39c945beec8bf058ab8e0c28b31822577c97655
jimmyNicholas <jimmynicholas@duck.com>
2024-10-27 18:49:41 +1100

    Added an upload file icon

--- src/_assets/icons.ts
+++ src/_assets/icons.ts
@@ -3,3 +3,6 @@ export const ResetIcon = ArrowPathIcon;
 
 import { BsKeyboard } from "react-icons/bs";
 export const QwertyIcon = BsKeyboard;
+
+import { TbUpload } from "react-icons/tb";
+export const UploadFileIcon = TbUpload;
