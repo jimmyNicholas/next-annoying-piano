4600047917a04fd0f17b9ef00eef16f66b417b36
jimmyNicholas <jimmynicholas@duck.com>
2024-10-27 13:01:27 +1100

    Updated to accept new Modes array

--- src/_hooks/useMode.ts
+++ src/_hooks/useMode.ts
@@ -1,31 +1,29 @@
 import { useCallback, useRef } from "react";
-import { modes } from "@/_lib/_data/modes";
 import { Mode } from "@/_lib/_types/types";
+import { modes } from '@/_lib/_data/modes/index' 
 
 const useMode = (
     onReset: () => void,
 ) => {
-    const modeIndex = useRef<number>(0);
-    const modeState = useRef<Mode>(modes[modeIndex.current]);
+    const modeRef = useRef<Mode>(modes[0]);
 
-    const getModeState = useCallback(() => {
-        return modeState.current;
+    const getModeRef = useCallback(() => {
+        return modeRef.current;
     }, []);
 
-    const setModeState = useCallback((newModeIndex: number) => {
-        modeIndex.current = newModeIndex;
-        modeState.current = modes[modeIndex.current];
+    const setModeRef = useCallback((newMode: Mode) => {
+        modeRef.current = newMode;
         onReset();
     },[onReset]);
 
-    const setMod = useCallback((newValue: number, index: number) => {
-        if (!modeState.current.modifiers) return;
-        const modifier = modeState.current.modifiers[index];
+    const updateModifier = useCallback((newValue: number, index: number) => {
+        if (!modeRef.current.modifiers) return;
+        const modifier = modeRef.current.modifiers[index];
         if (newValue < modifier.min || newValue > modifier.max) return;
-        modeState.current.modifiers[index].value = newValue;
+        modeRef.current.modifiers[index].value = newValue;
     }, []);
 
-    return { getModeState, setModeState, setMod, modes };
+    return { getModeRef, setModeRef, updateModifier, modes };
 };
 
 export default useMode;
