0000689c973283a1c15dd35e411ff98859ddb8c7
jimmyNicholas <jimmynicholas@duck.com>
2024-10-27 13:06:08 +1100

    Added 'id' for consistency

--- src/_lib/_data/modes/Mode.ts
+++ src/_lib/_data/modes/Mode.ts
@@ -1,15 +1,18 @@
 import { HertzModifiers, HertzTable, ModeModifiers } from '@/_lib/_types/types';
 
 export abstract class Mode {
+    readonly id: string;
     readonly name: string;
     readonly description: string;
     readonly modifiers: ModeModifiers[];
 
     constructor(
+        id: string,
         name: string,
         description: string,
         modifiers: ModeModifiers[]
     ) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.modifiers = modifiers;
