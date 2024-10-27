c22b7bcdbf716c05c8c2ca88951cf44e28615e21
jimmyNicholas <jimmynicholas@duck.com>
2024-10-27 18:27:37 +1100

    Imported / exported icon for qwerty keyboard

--- src/_assets/icons.ts
+++ src/_assets/icons.ts
@@ -1,3 +1,5 @@
 import { ArrowPathIcon } from "@heroicons/react/16/solid";
-
 export const ResetIcon = ArrowPathIcon;
+
+import { BsKeyboard } from "react-icons/bs";
+export const QwertyIcon = BsKeyboard;
