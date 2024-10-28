62a5a5d0b4cc1d4cb0eebc86cc0eee12feff8656
jimmyNicholas <jimmynicholas@duck.com>
2024-10-28 11:25:48 +1100

    Updated modes index and adjusted order

--- src/_lib/_data/modes/index.ts
+++ src/_lib/_data/modes/index.ts
@@ -1,8 +1,10 @@
 import { Mode } from "@/_lib/_types/types";
-import { GravityMode } from "./gravity";
 import { SwapMode } from "./swap";
+import { GravityMode } from "./gravity";
+import { MoveMode } from "./move";
 
 export const modes: Mode[] = [
     new SwapMode(),
-    new GravityMode()
+    new GravityMode(),
+    new MoveMode()
 ];
