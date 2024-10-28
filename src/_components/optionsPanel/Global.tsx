909e45b2a02b33d6d6b78ec64fd7aaa7e906a44d
jimmyNicholas <jimmynicholas@duck.com>
2024-10-28 22:02:27 +1100

    Removed legacy code

--- src/_components/optionsPanel/Global.tsx
+++ src/_components/optionsPanel/Global.tsx
@@ -6,12 +6,6 @@ const Global: React.FC<GlobalProps> = ({
 }) => {
     return (
         <div className="border-2 border-black grid grid-cols-2">
-            {/* <button
-                key={'enableAudio'}
-                className={`${audioIsLoaded ? "bg-yellow-300" : "bg-slate-300"} m-2`}
-            >
-                Audio Enabled
-            </button> */}
             <button
                 key={'reset'}
                 className="bg-rose-400 m-2 active:bg-rose-300 flex justify-center"
