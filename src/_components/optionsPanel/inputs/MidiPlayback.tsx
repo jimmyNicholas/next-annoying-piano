81b3bc7ad8207ddd95020d1ad638e46e3d1e0577
jimmyNicholas <jimmynicholas@duck.com>
2024-11-02 22:07:00 +1100

    Updated UI and layout

--- src/_components/optionsPanel/inputs/MidiPlayback.tsx
+++ src/_components/optionsPanel/inputs/MidiPlayback.tsx
@@ -12,29 +12,29 @@ const MidiPlayback: React.FC<MidiPlaybackProps> = ({
   }) => {
 
     return (
-        <div>
-            <div className="grid grid-flow-col">         
+        <div className="flex-1 grid grid-rows-2 gap-2 p-2">
+            <div className="grid grid-flow-col gap-1">         
                 <button 
-                    className={`${playbackState === 'playing' ? "bg-yellow-300" : "bg-slate-300"} border-2 border-black p-1 grid justify-center content-center`}
+                    className={`${playbackState === 'playing' ? "bg-yellow-300" : "bg-slate-300"} p-2 place-items-center rounded-lg`}
                     onClick={play}>
-                        <PlayIcon className="size-12"/> 
+                        <PlayIcon className="w-10 h-10"/> 
                 </button>
                 <button 
-                    className={`${playbackState === 'paused' ? "bg-yellow-300" : "bg-slate-300"} border-2 border-black p-1 grid justify-center content-center`}
+                    className={`${playbackState === 'paused' ? "bg-yellow-300" : "bg-slate-300"} p-2 place-items-center rounded-lg`}
                     onClick={pause}>
-                        <PauseIcon className="size-12"/>
+                        <PauseIcon className="w-10 h-10"/>
                 </button>
                 <button 
-                    className={`${playbackState === 'stopped' ? "bg-yellow-300" : "bg-slate-300"} border-2 border-black p-1 grid justify-center content-center`}
+                    className={`${playbackState === 'stopped' ? "bg-yellow-300" : "bg-slate-300"} p-2 place-items-center rounded-lg`}
                     onClick={stop}>
-                        <StopIcon className="size-12"/>
+                        <StopIcon className="w-10 h-10"/>
                 </button>
             </div>
             <div className="grid grid-flow-col">
                 <label
-                    className={`bg-slate-300 grid justify-center content-center`}
+                    className={`bg-slate-300 grid justify-center content-center rounded-lg`}
                 >
-                    <UploadFileIcon className="size-20"/>
+                    <UploadFileIcon className="w-16 h-16"/> 
                     <input 
                         type="file" 
                         accept=".mid, .midi" 
